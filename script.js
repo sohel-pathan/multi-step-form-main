e8f17760bad5cf73bfc715cc4f4d68cddafa95c6
sohel08 <sohelpathan2394@gmail.com>
2022-12-17 11:53:45 +0530

    format js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-// select sidebar step
-const sidebarStep = document.getElementsByClassName('indecater__num');
-// select form step
-const formStep = document.getElementsByClassName('step');
+// declare variables and store elements
+const sidebarStep = document.querySelectorAll('.indecater__num');
+const formStep = document.querySelectorAll('.step');
+const form = document.getElementById('form');
+const planCards = document.querySelectorAll('.plan__card');
+const addsonCards = document.querySelectorAll('.addon__card');
 const changePlanBtn = document.getElementById('change-plan');
-
-// object to store selected plan and price
+// empty object to store selected plan, price and duration
 let selectedPlan = {};
-// function for store selected add-on and price in a array
+// function for storing selected add-on plan
 const selectedAddsOn = () => {
+  // create emplty array to store plan details
   let addOnArr = [];
+  // loop add-on card to find selected add-on cards
   addsonCards.forEach((card) => {
+    // select plan name, price and plan duration
     let price = card.querySelector('.sbscription__price').textContent;
     let name = card.querySelector('.card__name').textContent;
     let planDur = card.querySelector('.sbscription__duration').textContent;
-    // checkbox checked of selected cards
+    // push selected cards details to addOnArr array
     if (card.classList.contains('selected')) {
       addOnArr.push({
         price,
@@ -22,59 +26,27 @@ const selectedAddsOn = () => {
       });
     }
   });
+  // return addOnArr array
   return addOnArr;
 };
-// FORM VALIDATTION
-const form = document.getElementById('form');
-const formInput = form.querySelectorAll('input');
 
-// function for add error
-const showError = (input, warningText) => {
-  input.classList.add('error');
-  input.parentElement.querySelector('.warning').textContent = warningText;
-};
+// plan prices
+const monthlyPlanPrices = [9, 12, 15];
+const yearlyPlanPrices = [90, 120, 150];
+const monthlyAdsOnPrice = [1, 2, 2];
+const yearlyAdsOnPrice = [10, 20, 20];
 
-// function for remove error
-const hideError = (input) => {
-  input.classList.remove('error');
-  input.parentElement.querySelector('.warning').textContent = '';
-};
-
-// show if plan not selected
-const selectPlanError = (text) => {
-  document.getElementById('select-plan-error').textContent = text;
-};
-
-// form validation
-const formValidation = () => {
-  // check all inputs using forEach loop
-  formInput.forEach((input) => {
-    // username
-    if (input.name === 'userName') {
-      return input.value.length === 0
-        ? showError(input, 'Enter your name')
-        : hideError(input);
-    }
-    // email
-    if (input.name === 'email') {
-      const emailRegExp = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
-      return input.value.length === 0
-        ? showError(input, 'Enter email')
-        : !emailRegExp.test(input.value)
-        ? showError(input, 'Enter valid email')
-        : hideError(input);
-    }
-    // phone number
-    if (input.name === 'phone') {
-      // const phoneRegExp = new RegExp(/^\+?[0-9]+$/);
-      return input.value.length === 0
-        ? showError(input, 'Enter your mobile number')
-        : hideError(input);
-    }
+// function to change price and duration of the given card
+const setplan = (card, price, duration) => {
+  card.forEach((card, i) => {
+    card.querySelector('.sbscription__price').textContent = `${price[i]}`;
+    card.querySelector('.sbscription__duration').textContent = `${duration}`;
   });
-  // return form validity
-  return form.checkValidity();
 };
+
+// set default price and duration of the cards
+setplan(planCards, monthlyPlanPrices, 'mo');
+setplan(addsonCards, monthlyAdsOnPrice, 'mo');
 
 // =====================
 // BUTTONS
@@ -83,7 +55,15 @@ const nextBtn = document.getElementById('next-button');
 const prevBtn = document.getElementById('prev-button');
 
 // NEXT STEP BUTTON
+// step number
 let stepNum = 0;
+
+// declare function for show warning text when plan not selected and next button pressed
+const selectPlanError = (text) => {
+  document.getElementById('select-plan-error').textContent = text;
+};
+
+// handle next button
 nextBtn.addEventListener('click', (e) => {
   e.preventDefault();
   if (stepNum === 0) {
@@ -109,29 +89,29 @@ nextBtn.addEventListener('click', (e) => {
   } else return;
 });
 
-// PREVIUOS STEP BUTTON
+// handle previus button
 prevBtn.addEventListener('click', (e) => {
   e.preventDefault();
   stepNum--;
   return showStep(stepNum);
 });
 
-// =====================
-//  SHOW CURRENT STEP
-// =====================
-// fuction for showing current steps of form and sidebar
+// fuction for handle steps
 const showStep = (x) => {
-  // remove plan select warning
+  // remove plan select warning text
   selectPlanError('');
-  // sidebar step
+
+  // handle sidebar step
   if (x < sidebarStep.length) {
+    // remove active class from all sidebar step
     for (let i = 0; i < sidebarStep.length; i++) {
       sidebarStep[i].classList.remove('active');
     }
-    // and adds the "active" class to the current sidebar step
+    // adds the "active" class to the current sidebar step
     sidebarStep[x].classList.add('active');
   }
-  // form step
+
+  // handle form step
   if (x < formStep.length) {
     if (x === 0) {
       // hide previuos button on the first step
@@ -154,30 +134,81 @@ const showStep = (x) => {
     for (let i = 0; i < formStep.length; i++) {
       formStep[i].classList.remove('active');
     }
-    // and adds the "active" class to the current form step
+    // adds the "active" class to the current step
     formStep[x].classList.add('active');
   }
 };
 // show first step by default
 showStep(stepNum);
 
-// STEP2
+// STEP-1 | PERSONAL INFO [ FORM-VALIDATION ]
+
+// function for showing warning on input
+const showError = (input, warningText) => {
+  input.classList.add('error');
+  input.parentElement.querySelector('.warning').textContent = warningText;
+};
+
+// function for remove warning of the input
+const hideError = (input) => {
+  input.classList.remove('error');
+  input.parentElement.querySelector('.warning').textContent = '';
+};
+
+// selecte all forms input
+const formInput = form.querySelectorAll('input');
+
+// function for form validation
+const formValidation = () => {
+  // check all inputs are valid using forEach loop
+  formInput.forEach((input) => {
+    // username
+    if (input.name === 'userName') {
+      return input.value.length === 0
+        ? showError(input, 'Enter your name')
+        : hideError(input);
+    }
+    // email
+    if (input.name === 'email') {
+      // verify email input value using regex
+      const emailRegExp = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+      return input.value.length === 0
+        ? showError(input, 'Enter email')
+        : !emailRegExp.test(input.value)
+        ? showError(input, 'Enter valid email')
+        : hideError(input);
+    }
+    // phone number
+    if (input.name === 'phone') {
+      return input.value.length === 0
+        ? showError(input, 'Enter your mobile number')
+        : hideError(input);
+    }
+  });
+  // return form validity
+  return form.checkValidity();
+};
+
+// STEP-2 | SELECT PLAN & TOGGLE BUTTON
 const toggle = document.getElementById('toggle');
-const planCards = document.querySelectorAll('.plan__card');
 const yearlyBenefit = document.querySelectorAll('.yearly__benefit');
 const month = document.getElementById('monthly');
 const year = document.getElementById('yearly');
 
-// toggle button
-
+// toggle button for changing plan duration yearly or monthly
 toggle.addEventListener('click', (e) => {
-  const toggle = e.target.parentElement;
   selectPlanError('');
-  // toggle.parentElement.parentElement.querySelectorAll('.plan__card');
-  toggle.classList.toggle('active');
+  // selelct toggle container
+  const toggle = e.target.parentElement;
+  // unselect selected plan card
   planCards.forEach((card) => card.classList.remove('selected'));
+  // make selectedPlan object emply
   selectedPlan = {};
-  // add condition to change plan monhtly or yearly and update data
+
+  // add active class to the toggle
+  toggle.classList.toggle('active');
+
+  // if toggle active [ yearly ] else [ monhtly ]
   if (toggle.classList.contains('active')) {
     yearlyBenefit.forEach((item) => item.classList.add('show'));
     setplan(planCards, yearlyPlanPrices, 'yr');
@@ -193,108 +224,109 @@ toggle.addEventListener('click', (e) => {
   }
 });
 
-// select plan
+// Select plan card
 planCards.forEach((card) => {
   card.addEventListener('click', (e) => {
     selectPlanError('');
     let target = e.currentTarget;
+    // remove selected class of all cards
+    planCards.forEach((card) => card.classList.remove('selected'));
+    // add selected class to the current card
+    target.classList.add('selected');
+
+    // store selected plan name, price and duration
     let planName = target.querySelector('.card__name').textContent;
     let planPrice = target.querySelector('.sbscription__price').textContent;
     let planDur = target.querySelector('.sbscription__duration').textContent;
-    selectedPlan = { planName, planPrice, planDur };
-    // remove selected class of all cards
-    planCards.forEach((card) => card.classList.remove('selected'));
-    // add selected class to the selected card
-    return target.classList.add('selected');
+
+    // add selected plan details to the selectedPlan object
+    return (selectedPlan = { planName, planPrice, planDur });
   });
 });
 
-// STEP3 | ADDS-ON
-const addsonCards = document.querySelectorAll('.addon__card');
+// STEP3 | ADD-ON
 addsonCards.forEach((card) => {
+  // addeventlistner to the add-on card
   card.addEventListener('click', (e) => {
     let target = e.currentTarget;
-    // select card
+    let checkbox = target.querySelector('.checkbox');
+    // add selected class to the card
     target.classList.toggle('selected');
 
-    // checkbox checked of selected cards
     if (target.classList.contains('selected')) {
-      return (target.querySelector('.checkbox').checked = true);
+      return (checkbox.checked = true);
     } else {
-      // console.log(selectedAddsOn);
-      return (target.querySelector('.checkbox').checked = false);
+      return (checkbox.checked = false);
     }
   });
 });
 
-// set plan price
-const monthlyPlanPrices = [9, 12, 15];
-const yearlyPlanPrices = [90, 120, 150];
-const monthlyAdsOnPrice = [1, 2, 2];
-const yearlyAdsOnPrice = [10, 20, 20];
+// STEP-4 | FINISHING UP
 
-const setplan = (card, plan, xx) => {
-  card.forEach((card, i) => {
-    card.querySelector('.sbscription__price').textContent = `${plan[i]}`;
-    card.querySelector('.sbscription__duration').textContent = `${xx}`;
-  });
-};
-setplan(planCards, monthlyPlanPrices, 'mo');
-setplan(addsonCards, monthlyAdsOnPrice, 'mo');
-
-// render last step
-
+// RENDER SELECTED PLAN, SELECTED ADD-ON AND TOTAL AMOUNT
 const renderTotal = () => {
-  let totalPrice = 0;
-  let planDuration = selectedPlan.planDur === 'mo' ? 'Monthly' : 'Yearly';
+  // make var to store total amount
+  let totalAmount = 0;
+  // plan duration in full-form
+  const planDuration = selectedPlan.planDur === 'mo' ? 'Monthly' : 'Yearly';
+  // selecte element to append selected plan. add-on and total price
   const plan = document.getElementById('selected-plan');
   const addsOnList = document.getElementById('selected-addon');
   const total = document.getElementById('total');
+
+  // clear innerHTML of the selected elements
   total.innerHTML = '';
   addsOnList.innerHTML = '';
   plan.innerHTML = '';
+
   // add selected plan
   let planName = document.createElement('p');
   planName.textContent = selectedPlan.planName;
 
-  let my = document.createElement('p');
-  my.textContent = `(${planDuration})`;
+  let dur = document.createElement('p');
+  dur.textContent = `(${planDuration})`;
 
   let planPrice = document.createElement('p');
   planPrice.textContent = `$${selectedPlan.planPrice}/${selectedPlan.planDur}`;
+  // append selected plan to the plan
   plan.appendChild(planName);
-  plan.appendChild(my);
+  plan.appendChild(dur);
   plan.appendChild(planPrice);
 
-  // add plan price in total
-  totalPrice += parseInt(selectedPlan.planPrice);
+  // add selectedplan price in total amount
+  totalAmount += parseInt(selectedPlan.planPrice);
 
-  // set selected add-on and their prices
+  // add selected add-on in addsOnList
   selectedAddsOn().forEach((item) => {
+    // create listItem to store selected add-on details
     let listItem = document.createElement('li');
+    // addOnName to store add-on title
     let addOnName = document.createElement('p');
     addOnName.textContent = item.name;
+    // addOnprice for add-on price and plan duration
     let addOnprice = document.createElement('p');
     addOnprice.textContent = `+$${item.price}/${item.planDur}`;
 
+    // append  addOnName and addOnprice to the listItem
     listItem.appendChild(addOnName);
     listItem.appendChild(addOnprice);
+    // add list item to then addOnlist
     addsOnList.appendChild(listItem);
 
-    // add selected add on price
-    totalPrice += parseInt(item.price);
+    // add price to the total price
+    totalAmount += parseInt(item.price);
   });
 
-  console.log(selectedAddsOn);
-
-  // set total amount
+  // set inner html for total
   total.innerHTML = `<span>Total(
     per ${planDuration.slice(0, -2).toLocaleLowerCase()}) </span> 
-      <span> $${totalPrice}/${selectedPlan.planDur}</span>`;
+      <span> $${totalAmount}/${selectedPlan.planDur}</span>`;
 };
 
-// change plan button
+// function to handle change button on 4th step
 changePlanBtn.addEventListener('click', () => {
+  // reassing stepNum to 0
   stepNum = 0;
+  // show stepNum
   showStep(stepNum);
 });
